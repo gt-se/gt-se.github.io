f359e1733b06b431ccf8dcc0ef324f9d96a5ec6a
tuan.hoang4 <tuan.hoang4@niteco.se>
2025-08-28 10:20:47 +0700

    Pardot form - init resources for floating label style

---
+++ ResourcesVI/javascript/GTPardotFormFloatingLabel.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+
+    // Wait for document to be fully loaded
+    document.addEventListener('DOMContentLoaded', function() {
+        console.log('Document is ready and PardotFormFloatingLabel script is loaded');
+    });
+})();
